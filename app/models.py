448df9ecb2c8bb6134c1712f3ba8642867d027ef
Zalelin3 <shilin@students.pitzer.edu>
2020-03-27 18:10:17 -0700

    update new tables

--- app/models.py
+++ app/models.py
@@ -30,6 +30,22 @@ class User(UserMixin, db.Model):
     last_name = db.Column(db.String(60), index=True)
     password_hash = db.Column(db.String(128))
     is_admin = db.Column(db.Boolean, default=False)
+    gender = db.Column(db.Boolean)
+    school = db.Column(db.Integer, index=True)
+    has_pet = db.Column(db.Boolean)
+    pets = db.relationship('Pet', backref='user', lazy=True)
+    is_smoke = db.Column(db.Boolean)
+    allergies = db.relationship('Allergy', backref='user', lazy=True)
+    has_car = db.Column(db.Boolean)
+    has_bike = db.Column(db.Boolean)
+    noise_Sen = db.Column(db.Integer, index=True)
+    wakeup = db.Column(db.Integer, index=True)
+    bedtime = db.Column(db.Integer, index=True)
+    partyFreq = db.Column(db.Integer, index=True)
+    visitorFreq = db.Column(db.Integer, index=True)
+    post_id = db.relationship('Post', backref='user', uselist=False)
+    status = db.relationship('Live', backref='user', uselist=False)
+    ghost_users = db.relationship('Ghost_User', backref='user', lazy=True)
 
     @property
     def password(self):
@@ -61,35 +77,141 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-# class Department(db.Model):
-#     """
-#     Create a Department table
-#     """
+class Pet(db.Model):
+    """
+    Create a Pet table
+    """
 
-#     __tablename__ = 'departments'
+    __tablename__ = 'pets'
 
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(60), unique=True)
-#     description = db.Column(db.String(200))
-#     employees = db.relationship('Employee', backref='department',
-#                                 lazy='dynamic')
+    name = db.Column(db.String(60), primary_key=True)
+    description = db.Column(db.String(200)) #should we add description?
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),
+        nullable=False, primary_key=True)
 
-#     def __repr__(self):
-#         return '<Department: {}>'.format(self.name)
+    def __repr__(self):
+        return '<Pet: {}>'.format(self.name)
 
+class Allergy(db.Model):
+    """
+    Create a Pet table
+    """
 
-# class Role(db.Model):
-#     """
-#     Create a Role table
-#     """
+    __tablename__ = 'allergies'
 
-#     __tablename__ = 'roles'
+    name = db.Column(db.String(60), primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),
+        nullable=False, primary_key=True)
 
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(60), unique=True)
-#     description = db.Column(db.String(200))
-#     employees = db.relationship('Employee', backref='role',
-#                                 lazy='dynamic')
+    def __repr__(self):
+        return '<Allergy: {}>'.format(self.name)
 
-#     def __repr__(self):
-#         return '<Role: {}>'.format(self.name)
+class Post(db.Model):
+    """
+    Create a Post table
+    """
+
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    post_type = db.Column(db.Boolean, default=True, index=True)
+    #True for on campus post, and False for off-campus post 
+    title = db.Column(db.String(60), nullable=False)
+    description = db.Column(db.String(200), nullable=False, index=False)
+    capacity = db.Column(db.Integer)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'),
+        nullable=False)
+    images= db.relationship('Image', backref= 'posts', lazy=True)
+    lives = db.relationship('Live', backref= 'posts', lazy=True)
+
+    def __repr__(self):
+        return '<Post: {}>'.format(self.id)
+
+class Ghost_User(db.Model):
+    """
+    Create a Ghost_User table
+    """
+    __tablename__ = 'ghost_user'
+
+    representer_id = db.Column(db.Integer, db.ForeignKey('user.id'),
+        nullable=False, primary_key=True)
+    email = db.Column(db.String(60), index=True, unique=True, primary_key=True)
+    first_name = db.Column(db.String(60), index=True)
+    last_name = db.Column(db.String(60), index=True)
+
+    def __repr__(self):
+        return '<Ghost_User: {}>'.format(self.email)
+
+class Image(db.Model):
+    """
+    Create a Image table
+    """
+    __tablename__ = 'images'
+
+    id = db.Column(db.Integer, primary_key=True)
+    images = db.Column(db.LargeBinary)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=False)
+
+    def __repr__(self):
+        return '<Images: {}>'.format(self.id)
+
+class On_campus(db.Model):
+    """
+    Create a on campus table
+    """
+    __tablename__ = 'oncampus'
+
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=False, primary_key=True)
+    college = db.Column(db.Integer, nullable=False)
+    dorm_1 = db.Column(db.String(60), index = True)
+    dorm_2 = db.Column(db.String(60), index = True)
+    dorm_3 = db.Column(db.String(60), index = True)
+    drawNo = db.Column(db.Integer)
+    nSingles = db.Column(db.Integer, index=True, default = 0)
+    nDoubles = db.Column(db.Integer, index=True, default = 0)
+    nTriples = db.Column(db.Integer, index=True, default = 0)
+    nQuads = db.Column(db.Integer, index=True, default = 0)
+    
+    def __repr__(self):
+        return '<On campus: {}>'.format(self.college, self.dorm_1, self.drawNo)
+
+class Off_campus(db.Model):
+    """
+    Create a off campus table
+    """
+    __tablename__ = 'offcampus'
+
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=False, primary_key=True)
+    price = db.Column(db.Integer, nullable=False)
+    street = db.Column(db.String(100), nullable =False)
+    city = db.Column(db.String(60), index = True, nullable =False)
+    state = db.Column(db.String(60), index = True, nullable =False )
+    country = db.Column(db.String(60), index = True, nullable =False)
+    zipcode = db.Column(db.Integer, index = True, nullable =False)
+    nParking = db.Column(db.Integer, index=True, default= 0)
+    nRoom = db.Column(db.Integer, index=True, default = 0)
+    nBathroom = db.Column(db.Integer, index=True, default = 0)
+    nKitchen = db.Column(db.Integer, index=True, default = 0)
+    size = db.Column(db.Integer, index=True, default = 0)
+    pet_allowed = db.Column(db.Boolean, default=False)
+    substance_free = db.Column(db.Boolean, default=False)
+    ac = db.Column(db.Boolean, default=False)
+    laundry = db.Column(db.Boolean, default=False)
+    dishwasher = db.Column(db.Boolean, default=False)
+    url = db.Column(db.String(100))
+
+    def __repr__(self):
+        return '<Off campus: {}>'.format(self.price)
+
+class Live(db.Model):
+    """
+    Create a Live table
+    """
+    
+    __tablename__ = 'lives'
+
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=False, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False, primary_key=True)
+    status = db.Column(db.Integer, nullable=False)
+    def __repr__(self):
+        return '<Off campus: {}>'.format(self.status)
