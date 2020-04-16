2222c86e4dd8e2eb637e4c330038161b28f85892
ejimenez17 <ejimenez@g.hmc.edu>
2020-04-15 19:18:07 -0700

    on campus post forms

--- app/post/views.py
+++ app/post/views.py
@@ -21,3 +21,19 @@ def make_post_offcampus():
     Render the page to make a post for off-campus housing
     """
     return render_template('post/make_post_offcampus.html', title="Make a Post")
+
+@post.route('/make-post-oncampus-suite')
+@login_required
+def make_post_oncampus_suite():
+    """
+    Render the page to make a post for on-campus suite housing
+    """
+    return render_template('post/make_post_oncampus_suite.html', title="Make a Post")
+
+@post.route('/make-post-oncampus-room')
+@login_required
+def make_post_oncampus_room():
+    """
+    Render the page to make a post for on-campus room housing
+    """
+    return render_template('post/make_post_oncampus_room.html', title="Make a Post")
